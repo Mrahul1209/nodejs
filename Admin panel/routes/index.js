bd282710e4d23efe75901c1b271874ee3e0e1d83
Rahul D Makwana <rm0539649@gmail.com>
2026-01-09 10:55:25 +0530

    Delete Admin panel directory
    Delete Admin panel directory

--- Admin panel/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addminForm } = require('../controllers/admin.controller');
-
-const route = express.Router();
-
-route.get('/', addminForm);
-
-
-module.exports = route;
